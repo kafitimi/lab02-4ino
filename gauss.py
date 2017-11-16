0718afd5278d51bf4e4e6d61e83a72e2b1ae44ca
4ino <vovka_rp@mail.ru>
2017-11-17 02:19:28 +0900

    ne robit (╯°□°）╯︵ ┻━┻

--- gauss.py
+++ gauss.py
@@ -16,9 +16,21 @@ def main(filename, r):
     sigma = 0.38*r
     gauss_dist = np.exp( -(dx*dx+dy*dy)/(2*sigma**2) ) / (2*pi*sigma**2)
     coeff = gauss_dist / np.sum(gauss_dist)
-
+    
     # код сюда ....
-    newimg = img
+    
+    w, h = img.size
+    a = np.array(img.getdata(), dtype=np.uint8).reshape(h, w)
+    b = np.zeros((h,w), dtype=np.uint8)
+    for i in range(r,h-r,r*2):
+        for j in range(r,w-r,r*2):
+            for y in range(-r,r):
+                for x in range(-r,r):
+                    b[i+y,j+x] = (a[i+y,j+x]*coeff[r+y,r+x])
+
+    
+    
+    newimg = Image.fromarray(b)
     newimg.show()
     newimg.save(filename+'.gaussblurred.png')
 
